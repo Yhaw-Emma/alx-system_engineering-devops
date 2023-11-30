819410aca80bf0fe624cc2cea61289e32ca8d5a8
Yhaw Emma <owusuapraku1914@gmail.com>
2023-11-30 03:32:16 +0000

    update to task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# installs flask from pip3
+# Install pip3 package
+package { 'python3-pip':
+  ensure => installed,
+}
 
+# Install Flask using pip3
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
-  }
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
