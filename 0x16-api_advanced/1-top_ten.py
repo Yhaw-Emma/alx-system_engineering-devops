9cfd86432fbf492a3f152c8f8b07fc2b8791e943
Yhaw Emma <owusuapraku1914@gmail.com>
2024-05-08 03:16:56 +0000

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
-"""
+"""Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit."""
 
 import requests
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts for a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit name.
-    """
+    """Prints the titles of the first 10 hot posts for a given subreddit."""
+    
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
