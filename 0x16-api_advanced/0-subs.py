9e4ec5bc4774a39e04b68ef2fa06aeac68a50fe5
Yhaw Emma <owusuapraku1914@gmail.com>
2024-05-09 20:04:00 +0000

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and returns the number of subscribers for a given subreddit."""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
